feec2e7207191ef35fc0518e5a0937f5d148a67b
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-10-28 22:29:54 +0600

    basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-[#5e464d]">
+        <p className="text-[#ffffffb3] font-semibold italic text-center text-5xl mb-2">
+          Hello Dapp
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
