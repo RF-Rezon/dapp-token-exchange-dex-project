268698485bf6cd3ca5b84e06ed6e42ea7fffb8dd
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-11-01 16:29:22 +0600

    Test for basic 4 things. Name, tracks sysbols, decimals, and total supply.

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.13;
 
 contract Token {
-    string public name = "Rejwan";
+    string public name = "Tej Token";
+    string public symbol = "Tejgaon College";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor ()  {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
+
+
 }
