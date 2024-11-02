73c3772c1e026be409717c83218346ce6a966e1a
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-11-02 16:49:46 +0600

    sending transfers without refactoring

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -7,9 +7,27 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    mapping(address => uint256) public balanceOf; // ✋ Here balanceOf means who contains how much tokens. 
+
     constructor ()  {
         totalSupply = 1000000 * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 
+    function transfer (address _to, uint256 _number_of_token) public returns (bool success){  // 🔴 _value
+
+    // balanceOf[msg.sender] = balanceOf[msg.sender].sub(_number_of_token);
+    // balanceOf[_to] = balanceOf[_to].add(_number_of_token);
+
+    // Better way is here: 
+
+    require(balanceOf[msg.sender] >= _number_of_token, "Insufficient balance");
+        
+        // Using `-` and `+` directly for subtraction and addition
+        balanceOf[msg.sender] -= _number_of_token;
+        balanceOf[_to] += _number_of_token;
+    
+       return true;
+    }
 
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,10 +1,12 @@
+const { send } = require('vite');
+
 const Token = artifacts.require('./Token');
 
 require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('Token', (accounts) => {
+contract('Token', ([deployer, reciver]) => {   // ⚡ From Accounts array i took users with my suitable name. It could be a b c or whatever. Without this, i’d need to remember which specific index in the array represents each role (like, accounts[0], accounts[1]). 
 
     let token; 
     const name = "Tej Token"; 
@@ -34,6 +36,30 @@ contract('Token', (accounts) => {
             const result = await token.totalSupply();
             result.toString().should.equal(totalSupply);
         });
+        it('tracks if set the balance of the deployer is right or wrong', async () => {
+            const result = await token.balanceOf(deployer);
+            result.toString().should.equal(totalSupply);
+        });
     });
+
+    describe('sending tokens', ()=> {
+        it('tranfer tokens', async ()=>{
+            let totalNumberOfTokens;    // 🔴 balanceOf
+            // 🚩 Before transfer
+            totalNumberOfTokens = await token.balanceOf(reciver);
+            console.log("Receiver token ammounts before transfer", totalNumberOfTokens.toString())
+            totalNumberOfTokens = await token.balanceOf(deployer);
+            console.log("Deployer token ammounts transfer", totalNumberOfTokens.toString())
+            // 🚩 Transfer
+            await token.transfer(reciver, '1000000000000000000000000' , {from: deployer}) // 👉 Paramiter was 2. But sending 3. The 3rd one is the Meta data where we can add additional info. It is possible because of the web3.js.
+             
+            // 🚩 After transfer
+            totalNumberOfTokens = await token.balanceOf(reciver);
+            console.log("Receiver token ammounts transfer", totalNumberOfTokens.toString())
+            totalNumberOfTokens = await token.balanceOf(deployer);
+            console.log("Deployer token ammounts transfer", totalNumberOfTokens.toString())
+        })
+
+    })
 });
 
