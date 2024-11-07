786a30bcea9fd4bf4bdc25b0f5fb7fbfa2995c6a
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-11-07 20:10:01 +0600

    approving tokens by the developer

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -8,32 +8,44 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
-    mapping(address => uint256) public balanceOf; // ✋ Here balanceOf means who contains how much tokens. 
+    mapping(address => uint256) public balanceOf; // ✋ Here balanceOf means who contains how much tokens.
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    // Events 
+    // Events
 
-    event Transfer(address from, address _to, uint256 _ammouts_of_tokens);
+    event Transfer(address indexed from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-    constructor ()  {
+    constructor() {
         totalSupply = 1000000 * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer (address _to, uint256 _ammouts_of_tokens) public returns (bool success){  // 🔴 _value
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_to != address(0));
+        require(balanceOf[msg.sender] >= _value);
 
-    // balanceOf[msg.sender] = balanceOf[msg.sender].sub(_ammouts_of_tokens);
-    // balanceOf[_to] = balanceOf[_to].add(_ammouts_of_tokens);
-
-    // Better way is here: 
-
-       require(_to != address(0));
-       require(balanceOf[msg.sender] >= _ammouts_of_tokens);
-        
         // Using `-` and `+` directly for subtraction and addition
-        balanceOf[msg.sender] -= _ammouts_of_tokens;
-        balanceOf[_to] += _ammouts_of_tokens;
-        emit Transfer(msg.sender, _to, _ammouts_of_tokens);
-       return true;
-    } 
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -7,7 +7,7 @@ require('chai')
     .should()
 
 
-contract('Token', ([deployer, receiver]) => {   // ⚡ From Accounts array i took users with my suitable name. It could be a b c or whatever. Without this, i’d need to remember which specific index in the array represents each role (like, accounts[0], accounts[1]). 
+contract('Token', ([deployer, receiver, spender]) => {   // ⚡ From Accounts array i took users with my suitable name. It could be a b c or whatever. Without this, i’d need to remember which specific index in the array represents each role (like, accounts[0], accounts[1]). 
 
     let token;
     const name = "Tej Token";
@@ -19,40 +19,39 @@ contract('Token', ([deployer, receiver]) => {   // ⚡ From Accounts array i too
     beforeEach(async () => {                           // "beforeEach" should always under "describe". But its also allowed. 
         token = await Token.new()
     })
-
+ 
     describe('deployment', () => {
-
         it('tracks the name', async () => {
             const result = await token.name();
             result.should.equal(name);
-        });
+        })
         it('tracks the symbol', async () => {
             const result = await token.symbol();
             result.should.equal(symbol);
-        });
+        })
         it('tracks the decimals', async () => {
             const result = await token.decimals();
             result.toString().should.equal(decimals);
-        });
+        })
         it('tracks the total supply', async () => {
             const result = await token.totalSupply();
             result.toString().should.equal(totalSupply.toString());
-        });
+        })
         it('tracks if set the balance of the deployer is right or wrong', async () => {
             const result = await token.balanceOf(deployer);
             result.toString().should.equal(totalSupply.toString());
-        });
-    })
+        })
+    });
 
     describe('sending tokens', () => {                       // >>>>>>>>>>>>>>>>>>>>> TOKEN IS LIKE MONEY !!! >>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        let ammout
+        let amount
         let result
 
         describe('success', async () => {
             beforeEach(async () => {
-                ammout = tokenFunc(100);
+                amount = tokenFunc(100);
                 // 🚩 Transfer
-                result = await token.transfer(receiver, ammout, { from: deployer })   // 👉 Paramiter was 2. But sending 3. The 3rd one is the Meta data where we can add additional info. It is possible because of the web3.js.
+                result = await token.transfer(receiver, amount, { from: deployer })   // 👉 Paramiter was 2. But sending 3. The 3rd one is the Meta data where we can add additional info. It is possible because of the web3.js.
             })
 
 
@@ -72,7 +71,7 @@ contract('Token', ([deployer, receiver]) => {   // ⚡ From Accounts array i too
                 const event = log.args;
                 event.from.toString().should.eq(deployer, 'from is correct')
                 event._to.toString().should.eq(receiver, 'receiver is correct')
-                event._ammouts_of_tokens.toString().should.eq(ammout.toString(), 'value is correct')  // eq is the short form of equal.
+                event._value.toString().should.eq(amount.toString(), 'value is correct')  // eq is the short form of equal.
             })
         })
 
@@ -80,21 +79,51 @@ contract('Token', ([deployer, receiver]) => {   // ⚡ From Accounts array i too
         describe('failure', async () => {
             it('rejects insufficient balances', async () => {
 
-                let invalidAmount = tokenFunc(100000000000); // Greater than total supply
+                let invalidAmount = tokenFunc(1000000000); // Greater than total supply
                 await token.transfer(receiver, invalidAmount, { from: deployer }).should.be.rejectedWith(EVM_REVERT);
 
-                invalidAmount = tokenFunc(100);
+                invalidAmount = tokenFunc(1);
                 await token.transfer(deployer, invalidAmount, { from: receiver }).should.be.rejectedWith(EVM_REVERT);
             });
 
-            it('tracks valid receipent', async ()=>{
-                await token.transfer(INVALID_ADDRESS, ammout, {from: deployer}).should.be.rejectedWith(EVM_REVERT);
+            it('tracks valid receipent', async () => {
+                await token.transfer(INVALID_ADDRESS, amount, { from: deployer }).should.be.rejectedWith(EVM_REVERT);
             });
         })
 
-    })
+    });
+
+    describe('approving tokens', () => {
+        let result;
+        let amount;
+
+        beforeEach(async () => {
+            amount = tokenFunc(100);
+            result = await token.approve(spender, amount, { from: deployer }); // Allowing the ammounts of tokens 'you' can transmit. I will hold this. 
+        })
+
+        describe('success', () => {
+            it('allocates on allowance for delegated token spending on exchange', async () => {
+                const allowance = await token.allowance(deployer, spender)
+                allowance.toString().should.eq(amount.toString());  // Now, i wll allow you if your transact money matched the pervious. 
+            })
+            it('emits a approval event', async () => {
+                const log = result.logs[0];
+                log.event.should.eq('Approval');
+                const event = log.args;
+                event.owner.toString().should.eq(deployer, 'owner is correct')
+                event._spender.toString().should.eq(spender, '_spender is correct')
+                event._value.toString().should.eq(amount.toString(), 'value is correct')
+            })
+
+        })
+        describe('failure', () => {
+            it('rejects invalid spenders', async () => {
+                await token.approve(INVALID_ADDRESS, amount, { from: deployer }).should.be.rejected;
+            })
+        })
+
+    });
 
 })
-
-
 
