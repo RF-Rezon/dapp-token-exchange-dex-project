e16d7bc253e5ff5b6b286c06393fc6de1bc51816
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-11-10 23:28:44 +0600

    Add todoes

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Exchange {}
+contract Exchange {
+    // [] Set the fee account
+    // [] Deposit ether
+    // [] Withdraw ether
+    // [] Deposit tokens
+    // [] Withdraw tokens
+    // [] Check balances
+    // [] Make order
+    // [] Cancel order
+    // [] Fill order
+    // [] Change fees
+}
