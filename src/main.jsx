1f398a0f3ad4873f13169f421bb9b42749eb2f1e
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-11-28 16:20:14 +0600

    basic redux setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import "./index.css";
+import configureStore from "./store/configureStore";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <Provider store={configureStore()}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
